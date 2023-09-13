3f86955263801a2129bc520afa2a872f867e7945
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2023-09-12 17:01:42 -0700

    Add basic page layout

--- src/index.js
+++ src/index.js
@@ -1,16 +1,47 @@
-import "./style.css";
+import './meyer-reset.css';
+import './style.css';
 import Book from './book';
 
 // Dummy content
-const ggs = new Book(9780393317558, "Guns, Germs, and Steel", "Jared Diamond");
+const ggs = new Book(9780393317558, 'Guns, Germs, and Steel', 'Jared Diamond');
 
-function buildPage() {
-  const body = document.querySelector("body");
-  const wrapper = document.createElement("div");
-  wrapper.setAttribute("id", "wrapper");
-  wrapper.textContent = JSON.stringify(ggs);
+function renderPage() {
+  const body = document.querySelector('body');
 
-  body.append(wrapper);
+  // Generate header
+  const header = document.createElement('div');
+  header.setAttribute('id', 'header');
+
+  const title = document.createElement('h1');
+  title.textContent = "TinyLib";
+  header.append(title);
+
+  // Generate wrapper
+  const wrapper = document.createElement('div');
+  wrapper.setAttribute('id', 'wrapper');
+
+  const heading = document.createElement('h2');
+  heading.textContent = "my books";
+
+  const bookList = generateBookList();
+
+  // Generate "add" button
+  const addButton = document.createElement('button');
+  addButton.setAttribute('id', 'add-button');
+  addButton.textContent = '+';
+
+  // Generate copyright footer
+  const copyright = document.createElement("p");
+  copyright.setAttribute("id", "copyright");
+  copyright.textContent = `© ${new Date().getFullYear()} Stephen Roe`;
+
+  // Append all elements
+  wrapper.append(heading, bookList);
+  body.append(header, wrapper, addButton, copyright);
 }
 
-buildPage();
+function generateBookList() {
+  return JSON.stringify(ggs);
+}
+
+renderPage();
