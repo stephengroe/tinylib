2c3849e1de5c5fc29b33c69dfa7ee7005b89b3b2
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2023-09-15 20:07:00 -0700

    Install ESlint and Prettier

--- src/index.js
+++ src/index.js
@@ -1,43 +1,40 @@
-import './meyer-reset.css';
-import './style.css';
-import {Book, library, addBook} from './library';
+import "./meyer-reset.css";
+import "./style.css";
+import { Book, library, addBook } from "./library";
 
 // Initial page render
 function renderPage() {
-  const body = document.querySelector('body');
+  const body = document.querySelector("body");
 
   // Generate header
-  const header = document.createElement('div');
-  header.setAttribute('id', 'header');
+  const header = document.createElement("div");
+  header.setAttribute("id", "header");
 
-  const title = document.createElement('h1');
+  const title = document.createElement("h1");
   title.textContent = "TinyLib";
   header.append(title);
 
   // Generate wrapper
-  const wrapper = document.createElement('div');
-  wrapper.setAttribute('id', 'wrapper');
+  const wrapper = document.createElement("div");
+  wrapper.setAttribute("id", "wrapper");
 
-  const heading = document.createElement('h2');
+  const heading = document.createElement("h2");
   heading.textContent = "My Books";
 
   // Generate library container and fill it
-  const libraryContainer = document.createElement('div');
-  libraryContainer.setAttribute('class', 'library-container');
+  const libraryContainer = document.createElement("div");
+  libraryContainer.setAttribute("class", "library-container");
   renderBookList(libraryContainer, library);
 
   wrapper.append(heading, libraryContainer);
 
   // Generate and bind "add" button
-  const addButton = document.createElement('button');
-  addButton.setAttribute('id', 'add-button');
-  addButton.textContent = '+';
-  addButton.addEventListener('click', () => {
+  const addButton = document.createElement("button");
+  addButton.setAttribute("id", "add-button");
+  addButton.textContent = "+";
+  addButton.addEventListener("click", () => {
     addBook();
-    renderBookList(
-      document.querySelector('.library-container'),
-      library
-    );
+    renderBookList(document.querySelector(".library-container"), library);
   });
 
   // Generate copyright footer
@@ -56,36 +53,36 @@ function renderBookList(container, bookList) {
   }
 
   const bookArray = generateBookList(bookList);
-  bookArray.forEach(book => container.append(book));
+  bookArray.forEach((book) => container.append(book));
 }
 
 // Iterate through list of books
 function generateBookList(bookList) {
   const bookArray = [];
 
-  bookList.forEach(book => {
-    const bookContainer = document.createElement('div');
-    bookContainer.setAttribute('class', 'book-container');
+  bookList.forEach((book) => {
+    const bookContainer = document.createElement("div");
+    bookContainer.setAttribute("class", "book-container");
 
-    const bookImage = document.createElement('div');
-    bookImage.setAttribute('class', 'book-image');
+    const bookImage = document.createElement("div");
+    bookImage.setAttribute("class", "book-image");
     bookImage.style.backgroundImage = `url(${book.imageUrl})`;
 
-    const bookData = document.createElement('div');
+    const bookData = document.createElement("div");
 
-    const bookTitle = document.createElement('h3');
-    bookTitle.setAttribute('class', 'book-title');
+    const bookTitle = document.createElement("h3");
+    bookTitle.setAttribute("class", "book-title");
     bookTitle.textContent = book.title;
 
-    const bookAuthor = document.createElement('p');
-    bookAuthor.setAttribute('class', 'book-author');
+    const bookAuthor = document.createElement("p");
+    bookAuthor.setAttribute("class", "book-author");
     bookAuthor.textContent = book.author;
 
     bookData.append(bookTitle, bookAuthor);
     bookContainer.append(bookImage, bookData);
     bookArray.push(bookContainer);
   });
-  
+
   return bookArray;
 }
 
--- src/library.js
+++ src/library.js
@@ -12,13 +12,23 @@ class Book {
 }
 
 // Create new book
-function addBook(){
-  const principles = new Book(9781501124020, 'Principles', 'Ray Dalio', 'https://m.media-amazon.com/images/I/41q+cPPAsgL._SY445_SX342_.jpg');
+function addBook() {
+  const principles = new Book(
+    9781501124020,
+    "Principles",
+    "Ray Dalio",
+    "https://m.media-amazon.com/images/I/41q+cPPAsgL._SY445_SX342_.jpg",
+  );
   library.push(principles);
 }
 
 // Dummy content
-const ggs = new Book(9780393317558, 'Guns, Germs, and Steel', 'Jared Diamond', 'https://m.media-amazon.com/images/I/51Psz1zS5ZL._AC_UF1000,1000_QL80_.jpg');
+const ggs = new Book(
+  9780393317558,
+  "Guns, Germs, and Steel",
+  "Jared Diamond",
+  "https://m.media-amazon.com/images/I/51Psz1zS5ZL._AC_UF1000,1000_QL80_.jpg",
+);
 library.push(ggs);
 
-export {Book, library, addBook};
+export { Book, library, addBook };
