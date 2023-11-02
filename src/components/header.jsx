022b471a5efacdad3052135ea1ca894133a98176
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2023-11-02 15:40:59 -0700

    Format header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
 import '../styles/header.css';
 
 function Header() {
   return (
-    <div id="header">
-      <p>TinyLib</p>
+    <div className="header-wrapper">
+      <header>
+        <p>TinyLib</p>
+      </header>
     </div>
   )
 }
